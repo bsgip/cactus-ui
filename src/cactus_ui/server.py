48b9564443915952a89eb08a6b0707800e775805
Mustafa Habib <mustafa.habib@anu.edu.au>
2025-04-08 20:24:57 +1000

    wip - integrate into orchestrator

--- src/cactus_ui/server.py
+++ src/cactus_ui/server.py
@@ -1,12 +1,14 @@
 """Python Flask WebApp Auth0 integration example"""
 
-import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
+import requests
 
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask, redirect, render_template, session, url_for
+from werkzeug.wrappers.response import Response
+
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -23,36 +25,42 @@ oauth.register(
     client_id=env.get("AUTH0_CLIENT_ID"),
     client_secret=env.get("AUTH0_CLIENT_SECRET"),
     client_kwargs={
-        "scope": "openid profile email",
+        "scope": "user:all",
     },
     server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
 )
 
+CACTUS_ORCHESTRATOR_BASEURL = env["CACTUS_ORCHESTRATOR_BASEURL"]
+CACTUS_ORCHESTRATOR_AUDIENCE = env["CACTUS_ORCHESTRATOR_AUDIENCE"]
 
 # Controllers API
 @app.route("/")
-def home():
+def home() -> str:
     return render_template(
         "home.html",
-        session=session.get("user"),
-        pretty=json.dumps(session.get("user"), indent=4),
     )
 
 
+@app.route("/run")
+def run() -> str:
+    resp = requests.get(CACTUS_ORCHESTRATOR_BASEURL + "/run" , headers={"Authorization": f"Bearer {session["user"]["access_token"]}"})
+    return render_template("run.html", run=resp.json()["items"])
+
+
 @app.route("/callback", methods=["GET", "POST"])
-def callback():
+def callback() -> Response:
     token = oauth.auth0.authorize_access_token()
     session["user"] = token
-    return redirect("/")
+    return redirect("/run")
 
 
 @app.route("/login")
-def login():
-    return oauth.auth0.authorize_redirect(redirect_uri=url_for("callback", _external=True))
+def login() -> str:
+    return oauth.auth0.authorize_redirect(redirect_uri=url_for("callback", _external=True), audience=CACTUS_ORCHESTRATOR_AUDIENCE)
 
 
 @app.route("/logout")
-def logout():
+def logout() -> Response:
     session.clear()
     return redirect(
         "https://"
@@ -69,4 +77,4 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=env.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=int(env.get("PORT", 3000)))
