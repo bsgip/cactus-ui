22178b4890d392f7c612137d46d68f13c85abd70
Mike Turner <mike.turner@anu.edu.au>
2026-07-28 11:10:51 +0800

    Use Page component in Admin Compliance Page

--- frontend/src/pages/AdminCompliancePage.tsx
+++ frontend/src/pages/AdminCompliancePage.tsx
@@ -1,15 +1,15 @@
-import { Flex, TextField } from '@radix-ui/themes';
+import { TextField } from '@radix-ui/themes';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useState } from 'react';
 
 import { fetchAdminComplianceRequests } from '../api/compliance';
 import { ErrorAlert } from '../components/ErrorAlert';
-import { PageHeader } from '../components/PageHeader';
 import { PageSpinner } from '../components/PageSpinner';
 import { useConfirm } from '../components/useConfirm';
 import { useDocumentTitle } from '../hooks/useDocumentTitle';
 import AdminComplianceRequestTable from '../components/AdminComplianceRequestTable';
 import type { AdminComplianceRequestResponse} from '../api/types';
+import Page from '../components/Page.tsx';
 
 
 export function AdminCompliancePage() {
@@ -30,13 +30,13 @@ export function AdminCompliancePage() {
 
 
   return (
-    <Flex direction="column" gap="3">
+    <Page title="Compliance" >
+
       {confirmDialog}
-      <PageHeader title="Compliance" />
 
       {actionError && <ErrorAlert message={actionError} />}
 
-      <TextField.Root
+      <TextField.Root mb="4"
         placeholder="Search by compliance request ID"
         value={filter}
         onChange={(e) => setFilter(e.target.value)}
@@ -49,7 +49,7 @@ export function AdminCompliancePage() {
       ) : (
         <AdminComplianceRequestTable requests={requests} filter={filter} refresh={refresh} setActionError={setActionError} requestPath={requestPath} confirm={confirm}/>
       )}
-    </Flex>
+    </Page>
   );
 }
 
