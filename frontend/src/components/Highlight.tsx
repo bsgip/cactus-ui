a750f0a7d67c9dc8e618ee9f66b5c79ba2bd90e8
Mike Turner <mike.turner@anu.edu.au>
2026-07-31 13:48:04 +0800

    Fix typing

--- frontend/src/components/Highlight.tsx
+++ frontend/src/components/Highlight.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 
-function Highlight({children, color}:{children: ReactNode, color:any}) {
+function Highlight({children, color}:{children: ReactNode, color: string}) {
   return (
     <span style={{backgroundColor:color, padding:"4px 8px", display: "inline-block", fontSize: "0.8rem"}}>{children}</span>
   );
